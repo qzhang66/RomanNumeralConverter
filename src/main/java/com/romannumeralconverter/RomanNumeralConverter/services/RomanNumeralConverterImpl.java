2b228128cde003e97c2c91611f752c7923aca6a2
Qing Zhang <qing.zhang.xp@gmail.com>
2022-04-12 16:48:43 -0600

    implement RomanNumeralConverter interface and add unit tests

--- src/main/java/com/romannumeralconverter/RomanNumeralConverter/services/RomanNumeralConverterImpl.java
+++ src/main/java/com/romannumeralconverter/RomanNumeralConverter/services/RomanNumeralConverterImpl.java
@@ -1,4 +1,56 @@
 package com.romannumeralconverter.RomanNumeralConverter.services;
 
-public class RomanNumeralConverterImpl {
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanNumeralConverterImpl implements RomanNumeralConverter {
+    private static final String[] THOUSAND_STRS = {"", "M", "MM", "MMM"};
+    private static final String[] HUNDRED_STRS = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    private static final String[] TEN_STRS = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    private static final String[] ONE_STRS = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    private static final Map<Character, Integer> ROMAN_LETTER_TO_NUM = new HashMap<Character, Integer>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
+    @Override
+    public int fromRomanNumeral(String romanNumeral) {
+        int sum = 0;
+        int length = romanNumeral.length();
+
+        for (int i = 0; i < length; i++) {
+
+            if (i < length - 1 &&
+                    ROMAN_LETTER_TO_NUM.get(romanNumeral.charAt(i)) < ROMAN_LETTER_TO_NUM.get(romanNumeral.charAt(i + 1))) {
+                sum = sum + (ROMAN_LETTER_TO_NUM.get(romanNumeral.charAt(i + 1)) - ROMAN_LETTER_TO_NUM.get(romanNumeral.charAt(i)));
+                i++;
+            } else {
+                sum = sum + ROMAN_LETTER_TO_NUM.get(romanNumeral.charAt(i));
+            }
+        }
+        return sum;
+    }
+
+    @Override
+    public String toRomanNumeral(int number) {
+        if (number < 1) {
+            throw new IllegalArgumentException("Error: Roman numerals cannot be zero or negative. number=" + number);
+        }
+        if (number > 3999) {
+            throw new IllegalArgumentException("Error: Roman numerals cannot exceed 3999.");
+        }
+
+        String thousands = THOUSAND_STRS[number / 1000];
+        String hundreds = HUNDRED_STRS[(number % 1000) / 100];
+        String tens = TEN_STRS[(number % 100) / 10];
+        String ones = ONE_STRS[number % 10];
+
+        return thousands + hundreds + tens + ones;
+    }
 }
+
---
+++ src/test/java/com/romannumeralconverter/RomanNumeralConverter/services/RomanNumeralConverterImplTest.java
@@ -0,0 +1,65 @@
+package com.romannumeralconverter.RomanNumeralConverter.services;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(JUnitParamsRunner.class)
+public class RomanNumeralConverterImplTest {
+
+    RomanNumeralConverterImpl romanNumeralConverterImpl = new RomanNumeralConverterImpl();
+
+    @Test
+    @Parameters({
+            "1, I",
+            "2, II",
+            "4, IV",
+            "15, XV",
+            "19, XIX",
+            "20, XX",
+            "37, XXXVII",
+            "109, CIX",
+            "446, CDXLVI",
+            "700, DCC",
+            "1234, MCCXXXIV",
+            "2999, MMCMXCIX",
+            "3000, MMM",
+            "3549, MMMDXLIX",
+            "3724, MMMDCCXXIV",
+            "3999, MMMCMXCIX"
+
+    })
+    public void testToRomanNumeralWhenGivenValidNumbers(int number, String expectedRomanNum) {
+        String actual = romanNumeralConverterImpl.toRomanNumeral(number);
+        assertEquals(expectedRomanNum, actual);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @Parameters({"0", "4000"})
+    public void testToRomanNumeralWhenGivenInvalidNumbers(int number) {
+        romanNumeralConverterImpl.toRomanNumeral(number);
+    }
+
+    @Test
+    @Parameters({
+            "I, 1",
+            "VIII, 8",
+            "XVI, 16",
+            "XXXIX, 39",
+            "XCIX, 99",
+            "CIX, 109",
+            "CDXLVI, 446",
+            "DCC, 700 ",
+            "MCCXXXIV, 1234",
+            "MMMCMXCIX, 3999"
+    })
+    public void testFromRomanNumeral(String romanNum, int expectedNum) {
+        int actual = romanNumeralConverterImpl.fromRomanNumeral(romanNum);
+        assertEquals(expectedNum, actual);
+
+    }
+}
